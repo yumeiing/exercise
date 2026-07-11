5f94b5db01d44d3935facc4a093ea052d2099d46
geji <yumeiingfromgeji@outlook.com>
2026-07-11 13:51:26 +0800

    SDL模板

---
+++ SDL_example/src/main.cpp
@@ -0,0 +1,39 @@
+#include <SDL2/SDL.h>
+
+#define WIDTH 480
+#define HEIGHT 320
+
+int main(int argc,char* argv[])
+{
+    if(SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("SDL init failed: %s",SDL_GetError());
+        return 1;
+    }
+
+    SDL_Window* window = SDL_CreateWindow("sdl-window",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,
+        WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
+    
+    if(!window)
+    {
+        SDL_Log("window init failed: %s",SDL_GetError());
+        return 1;
+    }
+
+    SDL_Renderer* render = SDL_CreateRenderer(window,-1,0);
+
+    if(!render)
+    {
+        SDL_Log("render init failed: %s",SDL_GetError());
+        return 1;
+    }
+
+    SDL_SetRenderDrawColor(render,0x00,0x00,0x00,0xff);
+    SDL_RenderClear(render);
+    SDL_RenderPresent(render);
+
+    SDL_Delay(3000);
+    SDL_DestroyRenderer(render);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
