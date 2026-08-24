dbefa89befe1c1059ca4becc9ef005475abc1db3
yumeiing <yumeiingfromgeji@outlook.com>
2026-08-24 22:29:19 +0800

    add(thread/):a thread exercise

--- README.md
+++ README.md
@@ -6,5 +6,6 @@
 |[SDL_example](./SDL_example)|c++|SDL2的模板|SDL2|
 |[raylib_example](./raylib_example)|c++|raylib模板|raylib|
 |[openURL](./openURL)|c++|啥也不是|raylib|
+|[thread](./thread)|c++|c++线程练习,其中使用了std::cout,可能导致输出混乱|c++标准库|
 ## 构建方式
 皆使用CMake。可使用文件中自带的CMakeLists.txt。
---
+++ thread/src/main.cpp
@@ -0,0 +1,66 @@
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <iostream>
+#include <cassert>
+
+/* "std::cout" if not safe to thread
+ * if you have c++23
+ * you can use <print>
+ * !: remeber change cmake file.
+ */
+//#include <print>
+
+std::vector<int> a(10000,0);
+int n,num = 0;
+
+void setup();
+void add1();
+
+int main()
+{
+    setup();
+    std::printf("setup over\n");
+    add1();
+}
+
+void setup()
+{
+    std::printf("请输入数字：");
+    std::cin>>n;
+    while(n>a.size() || n<0)
+    {
+        std::printf("请重新输入(1~10000)：");
+        std::cin>>n;
+    }
+}
+
+void add1()
+{
+    if(num >= n) return ;
+
+    std::thread tmp(add1);
+
+    std::mutex m;
+    auto id_class = std::this_thread::get_id();
+    if(m.try_lock())
+    {
+        std::cout<<id_class<<":get lock\n";
+        //std::println("{}:get lock",id_class);
+        ++num;
+        std::cout<<"num:"<<id_class<<std::endl;
+        //std::println("num:{}",num);
+        m.unlock();
+        std::cout<<id_class<<":unlock\n";
+        //std::println("{}:unlock",id_class);
+    }
+    else
+    {
+        std::cout<<id_class<<":try_lock failed\n";
+        //std::println("{}:try_lock failed",id_class);
+    }
+
+
+    assert(tmp.joinable());
+    tmp.join();
+}
